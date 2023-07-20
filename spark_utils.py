3eb92d2ef6d64f2f1e87949e9e8c6b83225bd645
phmot <paulo@kissplatform.com>
2023-07-20 16:39:23 +0200

    Change docstrings reStructuredText (reST) format

--- spark_utils.py
+++ spark_utils.py
@@ -7,17 +7,18 @@ import os
 from datetime import datetime
 from typing import List
 
-
 class SparkUtils:
-    """Class with all generic functions using spark."""
+    """
+    Class with all generic functions using spark.
+    """
 
-    def __init__(self,app_name:str) -> None:
-        """Contructor class
-
-        Args:
-            app_name (str): spark app name
+    def __init__(self, app_name:str) -> None:
         """
+        Constructor class.
 
+        :param app_name: spark app name
+        :type app_name: str
+        """
         self.spark = SparkSession \
             .builder \
             .appName(app_name) \
@@ -27,32 +28,31 @@ class SparkUtils:
             .config("spark.local.dir","tmp")\
             .getOrCreate()
         self.spark.sparkContext.setLogLevel("ERROR")
-        pass
 
     def read_csv_to_spark_dataframe(self, path:str) -> DataFrame:
-        """Read a csv into spark dataframe.
+        """
+        Read a csv into spark dataframe.
 
-        Args:
-            path (str): file path
-
-        Returns:
-            DataFrame: spark dataframe
+        :param path: file path
+        :type path: str
+        :return: spark dataframe
+        :rtype: DataFrame
         """ 
         return self.spark.read.option("delimiter", ",").option("header", "true").csv(path)
     
-    def rename_columns_from_dataframe(self,dataframe: DataFrame, old_columns: List[str], new_columns: List[str]) -> DataFrame:
-        """Rename columns from spark dataframe.
+    def rename_columns_from_dataframe(self, dataframe: DataFrame, old_columns: List[str], new_columns: List[str]) -> DataFrame:
+        """
+        Rename columns from spark dataframe.
 
-        Args:
-            dataframe (DataFrame): spark dataframe
-            old_columns (List[str]): list of current column names.
-            new_columns (List[str]): list of new column mames.
-
-        Raises:
-            ValueError: String with error.
-
-        Returns:
-            DataFrame: spark Dataframe with columns renamed.
+        :param dataframe: spark dataframe
+        :type dataframe: DataFrame
+        :param old_columns: list of current column names.
+        :type old_columns: List[str]
+        :param new_columns: list of new column names.
+        :type new_columns: List[str]
+        :raises ValueError: String with error.
+        :return: spark Dataframe with columns renamed.
+        :rtype: DataFrame
         """
         # Check if the lengths of old_columns and new_columns lists are the same
         if len(old_columns) != len(new_columns):
@@ -70,51 +70,57 @@ class SparkUtils:
 
         return dataframe
     
-    def drop_columns_from_dataframe(self,dataframe: DataFrame, columns_to_drop: List[str]) -> DataFrame:
-        """drop columns from a spark dataframe.
+    def drop_columns_from_dataframe(self, dataframe: DataFrame, columns_to_drop: List[str]) -> DataFrame:
+        """
+        Drop columns from a spark dataframe.
 
-        Args:
-            dataframe (DataFrame): spark dataframe
-            columns_to_drop (List[str]): list of column names.
-
-        Returns:
-            DataFrame: spark dataframe.
+        :param dataframe: spark dataframe
+        :type dataframe: DataFrame
+        :param columns_to_drop: list of column names.
+        :type columns_to_drop: List[str]
+        :return: spark dataframe.
+        :rtype: DataFrame
         """
         # Drop the specified columns from the DataFrame
         dataframe = dataframe.drop(*columns_to_drop)
         return dataframe
     
-    def filter_dataframe_by_country(self,dataframe: DataFrame, country_column: str, countries: list) -> DataFrame:
-        """filter dataframe by country column and list of countries.
+    def filter_dataframe_by_country(self, dataframe: DataFrame, country_column: str, countries: list) -> DataFrame:
+        """
+        Filter dataframe by country column and list of countries.
 
-        Args:
-            dataframe (DataFrame): spark Dataframe.
-            country_column (str): target column about countries.
-            countries (list): list of countries.
-
-        Returns:
-            DataFrame: spark Dataframe.
+        :param dataframe: spark Dataframe.
+        :type dataframe: DataFrame
+        :param country_column: target column about countries.
+        :type country_column: str
+        :param countries: list of countries.
+        :type countries: list
+        :return: spark Dataframe.
+        :rtype: DataFrame
         """
         # Use the filter method to filter the DataFrame based on the country_column
         filtered_dataframe = dataframe.filter(dataframe[country_column].isin(countries))
         return filtered_dataframe
     
-    def join_two_dataframes(self, first_dataframe:DataFrame,second_dataframe:DataFrame,key_column:str,join_type:str) -> DataFrame:
-        """Join two spark dataframe by a column key.
+    def join_two_dataframes(self, first_dataframe: DataFrame, second_dataframe: DataFrame, key_column: str, join_type: str) -> DataFrame:
+        """
+        Join two spark dataframe by a column key.
 
-        Args:
-            first_dataframe (DataFrame): spark Dataframe
-            second_dataframe (DataFrame): spark Dataframe
-            key_column (str): key column.
-            join_type (str): join type(inner,left,right).
-
-        Returns:
-            DataFrame: spark Dataframe.
+        :param first_dataframe: spark Dataframe
+        :type first_dataframe: DataFrame
+        :param second_dataframe: spark Dataframe
+        :type second_dataframe: DataFrame
+        :param key_column: key column.
+        :type key_column: str
+        :param join_type: join type (inner, left, right).
+        :type join_type: str
+        :return: spark Dataframe.
+        :rtype: DataFrame
         """
         return first_dataframe.join(second_dataframe, key_column, join_type)
 
-   
     def destroy_spark_connection(self):
-        """Stop spark connection.
+        """
+        Stop spark connection.
         """
         self.spark.stop()
